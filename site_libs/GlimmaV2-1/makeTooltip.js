912e474626d2dbdabaa023574e0de26d6e8a8dae
S <xz87@iu.edu>
2023-06-02 22:06:17 -0400

    update

--- site_libs/GlimmaV2-1/makeTooltip.js
+++
@@ -1,9 +0,0 @@
-function makeVegaTooltip(columns)
-{
-    // generate tooltip object for embedding in spec
-    let tooltipString = "{";
-    columns.forEach(x => tooltipString += `'${x}':datum['${x}'],`);
-    tooltipString += "}";
-    var tooltip = { "signal" : tooltipString };
-    return tooltip;
-}
